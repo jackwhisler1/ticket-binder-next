4ecc2098e75de9cad9a2fb18ab9c36692db4c7c9
Jack Whisler <78163023+jackwhisler1@users.noreply.github.com>
2024-06-18 12:23:10 -0500

    formatting for tickets

--- src/pages/components/TicketStub.tsx
+++ src/pages/components/TicketStub.tsx
@@ -1,44 +1,37 @@
 import React from 'react';
 import Concert from '@/types/Concert';
+import './styles.css';
+import { convertDateToUSFormat } from './helpers';
 type TicketStubProps = {
   concert: Concert; 
 };
 const TicketStub: React.FC<TicketStubProps> = ({ concert }) => {
-  const { eventDate, artist, venue, tour, sets, info } = concert;
-  console.log(concert);
+  const eventDate = convertDateToUSFormat(concert.eventDate); 
   return (
-    <div className="bg-white rounded-lg shadow-md p-6 w-full max-w-md mb-3">
-      {/* Header */}
-      <div className="flex items-center justify-between mb-4">
-        <div className="text-lg font-bold">{artist?.name && artist.name}</div>
-        <div className="text-sm font-semibold">{eventDate}</div>
+    <div className="ticket">
+      <div className="date">
+        <span className="day">{new Date(eventDate).getDate()}</span>
+        <span className="month-and-time">
+          {new Date(eventDate).toLocaleString('default', { month: 'short' }).toUpperCase()}
+          <br />
+          <span className="small">{eventDate}</span>
+        </span>
       </div>
-
-      {/* Venue and Tour */}
-      <div className="mb-4">
-        <div className="text-gray-600">{venue?.city.name}, {venue?.city.state}</div>
-        <div className="text-gray-600">{venue?.name}</div>
-        <div className="mt-2 text-sm font-semibold">Tour: {tour?.name}</div>
+      <div className="artist">
+        <span className="name">{concert.artist.name}</span>
+        <br />
+        <span className="live small">{concert.tour?.name}</span>
       </div>
-
-      {/* Sets */}
-      <div className="mb-4">
-        {sets?.set.map((set, index) => (
-          <div key={index} className="mb-2">
-            {set.encore && <div className="text-sm text-gray-600">Encore</div>}
-            <ul className="list-disc pl-4">
-              {set.song.map((song, idx) => (
-                <li key={idx}>{song.name}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
+      <div className="location">
+        <span>{concert.venue.city.name}</span>
+        <br />
+        <span className="small">{concert.venue.name}</span>
       </div>
-
-      {/* Additional Info */}
-      <div className="text-sm text-gray-600">{info}</div>
+      <div className="rip"></div>
+      <div className="cta">
+        <button className="buy">GRAB A TICKET</button>
+      </div>
     </div>
   );
 };
-
 export default TicketStub;
